8f31143ef98c096ca75e9b0d08c8a5f134725b7b
Nathan <nathaniwilliamsuk@gmail.com>
2023-04-04 20:11:48 +0100

    update syntax

--- js/App.js
+++ js/App.js
@@ -1,9 +1,11 @@
-Vue.component("tab-posts", {
+const TabPosts = {
     template: '<div><h1 class="title">Posts</h1><h2 class="subtitle">Some content coming soon.</h2></div>'
-});
-Vue.component("tab-archive", {
+}
+const TabArchive = {
     template: '<div><h1 class="title">Archive</h1><h2 class="subtitle">Some content coming soon.</h2></div>'
-});
+}
+
+export { TabPosts, TabArchive } 
 
 new Vue({
     el: "#app",
