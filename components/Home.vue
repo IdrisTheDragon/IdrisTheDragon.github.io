8cb25693b70c3f897bc59b23a923e86382ec438b
Nathan <nathaniwilliamsuk@gmail.com>
2023-04-04 20:37:40 +0100

    Add menu idea generator link

--- components/Home.vue
+++ components/Home.vue
@@ -2,6 +2,9 @@
     <div>
         <h1 class="title">Home</h1>
         <h2 class="subtitle">Some content coming soon.</h2>
+        
+        <a class="button is-primary" href="https://idristhedragon.tech/MenuIdeaGenerator/">Menu Idea Generator</a>
+        
     </div>
 </template>
 
