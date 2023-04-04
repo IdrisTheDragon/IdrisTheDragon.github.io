4795c1c8fa01b594bef154bd30eaa765ed620547
Nathan <nathaniwilliamsuk@gmail.com>
2023-04-04 20:14:50 +0100

    update syntax

--- js/App.js
+++ js/App.js
@@ -1,11 +1,11 @@
+export { TabPosts, TabArchive } 
+
 const TabPosts = {
     template: '<div><h1 class="title">Posts</h1><h2 class="subtitle">Some content coming soon.</h2></div>'
 }
 const TabArchive = {
     template: '<div><h1 class="title">Archive</h1><h2 class="subtitle">Some content coming soon.</h2></div>'
 }
-
-export { TabPosts, TabArchive } 
 
 new Vue({
     el: "#app",
