21607a04678ce70754220ec2377df1d341689644
Nathan <nathaniwilliamsuk@gmail.com>
2023-04-04 20:40:26 +0100

    some extra content

--- components/Home.vue
+++ components/Home.vue
@@ -2,7 +2,7 @@
     <div>
         <h1 class="title">Home</h1>
         <h2 class="subtitle">Some content coming soon.</h2>
-        
+        <p>In the mean time checkout this webthing I created:</p>
         <a class="button is-primary" href="https://idristhedragon.tech/MenuIdeaGenerator/">Menu Idea Generator</a>
         
     </div>
